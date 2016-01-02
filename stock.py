53b156d5437124668a0a5088d65e1b71b929db31
Lorenzo <tunedconsulting@gmail.com>
2016-01-02 21:02:57 +0100

    Implement ordered price history in Stock()

--- stock.py
+++ stock.py
@@ -5,19 +5,26 @@ __author__ = 'Lorenzo'
 class Stock:
     def __init__(self, symbol):
         self.symbol = symbol
-        self.price_history = []   # list of tuples
+        # list of tuples list[0] is most recent
+        self.price_history = []
 
     def update(self, timestamp, price):
         """Update price"""
         if not price > 0:
             raise ValueError('Price must be non-zero positive')
-        return self.price_history.append((timestamp, price, ))
+        price_history = self.price_history + [(timestamp, price, )]
+        # order updates from most recent, handles late updates
+        return setattr(
+            self,
+            'price_history',
+            sorted(price_history, key=lambda x: x[0], reverse=True)
+        )
 
     @property
     def trend(self):
         """Return a trend (the last three prices)"""
         # take the last 3 elements and reverse them, return list of prices
-        return [p[1] for p in self.price_history[-3:][:]]
+        return [p[1] for p in self.price_history[:2]]
 
     def trend_is_incremental(self):
         """Return:
@@ -27,9 +34,9 @@ class Stock:
         t = self.trend
         gen = (
             True
-            if t[i] < t[i+1]
+            if t[i] > t[i+1]
             else False
-            for i in range(len(t)) if i != 2
+            for i in range(len(t)-1)
         )
         if all(gen) is True:
             return True
@@ -40,4 +47,4 @@ class Stock:
 
     @property
     def price(self):
-        return self.price_history[-1][1] if self.price_history else None
+        return self.price_history[0][1] if self.price_history else None
