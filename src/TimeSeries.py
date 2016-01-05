3e10161bcb1efa93d75da6909d8dc744f4e710b1
Lorenzo <tunedconsulting@gmail.com>
2016-01-05 17:22:19 +0100

    Implement TimeSeries(): Update object as namedtuple, STMA and LTMA

--- src/TimeSeries.py
+++ src/TimeSeries.py
@@ -1,2 +1,96 @@
 # coding=utf-8
+from datetime import datetime, timedelta
+from collections import namedtuple
+
 __author__ = 'Lorenzo'
+
+# Create a named tuple to represent price updates in the history
+Update = namedtuple('Update', ['timestamp', 'price'])
+
+
+class TimeSeries():
+    """Provide time series and DMAC analysis"""
+    def __init__(self, stock):
+        # Turn tuples in to namedtuples for better handling
+        self.history = [
+            Update(
+                timestamp=p[0],
+                price=p[1]
+            )
+            for p in stock.price_history
+        ]
+
+    def calculate_stma_ltma(self, on_date, period='short'):
+        """
+        Calculate the average price of the previous 5 or 10 days from
+        the given date.
+
+        Example:
+           >>> # calculate STMA
+           >>> calculate_stma_ltma(date.today(), 'short')
+           >>> # calculate LTMA
+           >>> calculate_stma_ltma(date.today(), 'long')
+
+
+        :param date on_date: the reference date, default is 5
+        :param str period: 'short' for STMA, 'long' for LTMA. Default: 'short'
+        :return: float
+        """
+        period = {
+            'short': 5,
+            'long': 10
+        }.get(period)
+        if period is None: raise ValueError('period can be only \'short\' or \'long\'')
+
+        # #todo: consider only closing prices
+        series = [
+            h.price
+            for h in self.history
+            if on_date - timedelta(period) <= h.timestamp < on_date
+        ]
+        return sum(series) / len(series)
+
+
+    def calculate_stma_ltma_time_series(self, on_date, delta):
+        """
+        Calculate the time series of the Short Term Moving Average.
+        Return the STMA for the five days before the given date.
+        Calculate STMA recursevely for the last give number of days.
+
+
+        :param date on_date:
+        :param int delta:
+        :return:
+        """
+        # slice price_history for the last five closing price
+        # reverse
+        #
+        raise NotImplemented()
+        """def calculate(date, series=[]):
+            if date == on_date:
+                return series
+
+            return calculate(date, series)
+
+        return tuple()"""
+
+
+    def get_crossover_signal(self, on_date):
+        """
+        Check if in a given date there is a buy/sell signal using DMAC analysis.
+
+        Documentation:
+            Consider a stock, with closing prices as shown above. First, we
+            calculate two moving average trends. The short-term (5-day) moving
+            (STMA) average is calculated by taking the moving average for a
+            short number of days. The long-term moving (LTMA) average is
+            calculated by taking the moving average for a longer number of days,
+            for example the moving average of the last 10 days.
+            If the STMA crosses below-to-above the LTMA, that is a buy signal.
+            If the STMA crosses above-to-below the LTMA, that is a sell signal.
+
+        :param datetime date:
+        :return: 1, -1, 0
+        """
+        stm_lower = on_date - timedelta(6)
+        ltm_lower = on_date -timedelta(11)
--- testing/tests_timeseries.py
+++ testing/tests_timeseries.py
@@ -1,10 +1,11 @@
 # coding=utf-8
 import unittest
-from datetime import datetime
+from datetime import date, timedelta
 
 __author__ = 'Lorenzo'
 
 from src.stock import Stock
+from src.timeseries import TimeSeries
 
 
 class StockDMAC(unittest.TestCase):
@@ -12,23 +13,49 @@ class StockDMAC(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         cls.stock = Stock("GOOG")
-        # create a price history
-        cls.trend_fixture(
-            [82, 79, 84.1, 85.6, 80.9, 81.2, 81.5, 81.7, 82.0, 82.2, 82.5]
-        )
-
-    def setUp(self):
-        pass
 
     def trend_fixture(self, prices):
         """Create new prices in price history from a list of Float.
     To be used in the tests below."""
-        return [self.stock.update(datetime(2015, 12, i+1), price=p)
-                for i, p in enumerate(prices)]
+        return [
+            self.stock.update(date.today() + timedelta(days=-i), price=p)
+            for i, p in enumerate(prices)
+        ]
+
+    def setUp(self):
+        # create a price history
+        self.trend_fixture(
+            [82, 79, 84.1, 85.6, 80.9, 81.2, 81.5, 81.7, 82.0, 82.2, 82.5]
+        )
+        # create a DMAC object for the stock
+        self.timeseries = TimeSeries(self.stock)
+
+    def test_should_create_time_series(self):
+        # print(self.stock.price_history)
+        self.assertTrue(len(self.stock.price_history) > 6)
+
+    def test_should_calculate_stma(self):
+        stma = self.timeseries.calculate_stma_ltma(date.today(), period='short')
+        print('STMA', stma)
+        self.assertAlmostEqual(stma, 82.16, delta=0.0001)
+
+    def test_should_raise_error_in_calculate(self):
+        with self.assertRaises(ValueError):
+            self.timeseries.calculate_stma_ltma(date.today(), period='abc')
+            self.timeseries.calculate_stma_ltma(date.today(), period=1)
+
+    def test_should_calculate_ltma(self):
+        ltma = self.timeseries.calculate_stma_ltma(date.today(), period='long')
+        print('LTMA', ltma)
+        self.assertAlmostEqual(ltma, 82.07, delta=0.0001)
 
     def tearDown(self):
-        pass
+        del self.timeseries
 
     @classmethod
     def tearDownClass(cls):
-        pass
+        del cls.stock
+
+
+if __name__ == "__main__":
+    unittest.main()
